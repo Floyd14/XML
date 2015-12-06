24d69bb51a09ff8d2558eb1a72d0ec5dcae53d99
Andrea <andreafloyd14@icloud.com>
2015-12-06 23:13:44 +0100

    HomeTime

--- TestXML/xlm_To_xlsx3.py
+++ TestXML/xlm_To_xlsx3.py
@@ -1,63 +1,84 @@
 # -*- coding: UTF-8 -*-
 
-"""
+"""Convert specific xml to xlsx.
+
+
 
 """
 
 import xml.etree.ElementTree
 import openpyxl
 
-class manObj():
+
+class ManObj:
     """
     Una lista [...] 
     
     iterabile = può essere richiamata in un for loop
     """
-    
-    def __init__ (self):
-        self.manObj = {}
-        
-    def __iter__(self):
-        
-    
-    def __init__(self, 
-                 num = 0,
-                 sorgente = "",
-                 destinazione = "",
-                 classe = "",
-                 lac = "",
-                 targhet = "",
-                 has_lac = False):
-        
-        # identificativo lista
-        self.num = num
-        # param in list
-        self.classe = classe
-        self.sorgente = sorgente
-        self.destinazione = destinazione
-        self.has_lac = has_lac
-        self.lac = lac
-        self.target = targhet
-        # Si crea
-        self.create()
 
-    def create(self, xmlfile = "./test1.xml"):
-        # node of xmlfile where there are manageobjects
-        node = xml.etree.ElementTree.parse(xmlfile).getroot()[0]
-        managed_objs = node.findall('{raml20.xsd}managedObject') # to improve with namespace ...
+    KEYS = ("ID", "Sorgente", "Destinazione", "Classe", "LAC", "Target")
 
-        # per tutti gli oggetti
-        for managed_obj in managed_objs:
-            name = managed_obj.attrib['name']
-            self.sorgente = name[:7]
-            self.destinazione = name[11:]
-            self.cls = managed_obj.attrib['class']
-            return self
+    def __init__(self):
 
+        for key in self.KEYS:
+            self.KEYS = None
 
+        self.has_lac = False
+
+    def get_managed_object_from_xml(self, xml_filename = './test1.xml', DEBUG=False):
+        '''
+        Set the xml file to parse and return a list of managed object or None
+
+        :param xml_filename: the xml to parse
+        :return: list of all object [<element>]
+        '''
+
+        # node of xmlfile where there are managed_objects
+        node = xml.etree.ElementTree.parse(xml_filename).getroot()[0]
+        # find all the managed_Object
+        managed_objs = node.findall("{raml20.xsd}managedObject")
+
+        # se esiste
+        if managed_objs:
+            # print out for debug
+            if DEBUG:
+                for index, obj in enumerate(managed_objs):
+                    print("Trovato elemento {:-4}: {}".format(index, obj))
+
+            # ritorno la managed_object list []
+            self.managed_object = managed_objs
+            print("Creata la lista {} contenente {} oggetti".format('XX', len(managed_objs)))
+            return self.managed_object
+
+        # se non trovo nulla
+        else:
+            print("Non sono stati trovati elementi.")
+            return None
+
+    def format_object(self):
+        '''
+        format the managed object list
+
+        :return: a well formatted managed object list
+        '''
+
+        # Controllo se esiste una lista di oggetti
+        if self.managed_objs:
+            for obj in self.managed_object:
+
+                # per ogni oggetto in lista
+                name = obj.attrib['name']
+                self.sorgente = name[:7]
+                self.destinazione = name[11:]
+
+                self.classe = obj.attrib['class']
+
+        else:
+            print("Non posso formattare una lista vuota.")
 
     def get_manObj(self):
-        manObj = {"Classe" : self.cls,
+        manObj = {"Classe" : self.classe,
                   "Sorgente" : self.sorgente,
                   "Destinazione" : self.destinazione,
                   "LAC" : self.lac,
@@ -66,4 +87,5 @@ class manObj():
 
         return manObj
 
-
+if __name__ == '__main__':
+    a = ManObj().get_managed_object_from_xml(DEBUG=True)
